a56296dba1dfab29fbe04284753589539e335aab
Akogwu24 <akogwu.emmanuel24@gmail.com>
2021-05-13 21:47:14 +0100

    UI completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import ProductCard from './components/ProductCard';
 
 function App() {
+  const [productItems, setProductItems] = useState([]);
+
+  const getProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const productData = await response.json();
+    console.log(productData);
+    setProductItems(productData);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
-    <div className='App'>
-      <ProductCard />
+    <div className=' text-center'>
+      <h1 className='py-5 px-3 text-4xl font-bold'>
+        These are the Items we Have for you today, Kinldy remove the ones you do
+        not want
+      </h1>
+      {productItems.map((product) => (
+        <ProductCard
+          image={product.image}
+          productName={`${product.title.split(' ')[0]} ${
+            product.title.split(' ')[1]
+          } ${product.title.split(' ')[2]}`}
+          productPrice={product.price}
+        />
+      ))}
+
+      <button className='mb-20 bg-green-500 py-3 px-5 text-xl rounded-lg my-5 hover:bg-green-400 ml-2 font-bold'>
+        Click here to Buy All Items Above
+      </button>
     </div>
   );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import grocery from '../assets/images/download.jpg';
 
-const ProductCard = () => {
+const ProductCard = ({ image, productName, productPrice }) => {
   return (
     <div className='p-10'>
-      <div className='text-center bg-red-200 p-10'>
-        <div className='mb-5'>
-          <img
-            src={grocery}
-            alt='product'
-            className='img mx-auto rounded-2xl'
-          />
+      <div className='text-center p-10'>
+        <div className='image-container mb-5'>
+          <img src={image} alt='product' className='img mx-auto rounded-2xl' />
         </div>
         <div>
-          <h3 className='font-bold text-4xl'>product name</h3>
-          <h3 className='font-bold my-5 text-2xl'>price</h3>
-          <button className='bg-red-500 py-3 px-5 text-2xl rounded-lg my-5 hover:bg-red-400'>
+          <h3 className='font-bold text-4xl'>{productName}</h3>
+          <h3 className='font-bold my-5 text-2xl'>$ {productPrice}</h3>
+          <button className='w-42 bg-red-500 py-3 px-5 text-xl rounded-lg my-5 hover:bg-red-400 mr-2 font-bold'>
             Remove Item
           </button>
+          {/* <button className='w-42 bg-green-500 py-3 px-5 text-xl rounded-lg my-5 hover:bg-green-400 ml-2 font-bold'> */}
+          {/* Buy Items */}
+          {/* </button> */}
         </div>
       </div>
     </div>
